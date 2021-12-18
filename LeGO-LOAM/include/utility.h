bed7bf2227642afa6e08acf2bfafe80ca107a53f
Ha-JH <hajeongho95@gmail.com>
2021-12-19 06:31:27 +0900

    utility.h modified

--- LeGO-LOAM/include/utility.h
+++ LeGO-LOAM/include/utility.h
@@ -59,13 +59,31 @@ extern const string fileDirectory = "/tmp/";
 // Using velodyne cloud "ring" channel for image projection (other lidar may have different name for this channel, change "PointXYZIR" below)
 extern const bool useCloudRing = true; // if true, ang_res_y and ang_bottom are not used
 
+// type1 and type 2
+// extern const int N_SCAN = 32;
+// extern const int Horizon_SCAN = 500;
+// extern const float ang_res_x = 360.0/float(Horizon_SCAN);
+// extern const float ang_res_y = 40.0/float(N_SCAN-1);
+// extern const float ang_bottom = 15.0+0.1;
+// extern const int groundScanInd = 15;
+// extern const float scanPeriod = 0.05;
+
+// type3 and type 4
+extern const int N_SCAN = 128;
+extern const int Horizon_SCAN = 1024;
+extern const float ang_res_x = 360.0/float(Horizon_SCAN);
+extern const float ang_res_y = 45.0/float(N_SCAN-1);
+extern const float ang_bottom = 22.5+0.1;
+extern const int groundScanInd = 10;
+extern const float scanPeriod = 0.1;
+
 // VLP-16
-extern const int N_SCAN = 16;
-extern const int Horizon_SCAN = 1800;
-extern const float ang_res_x = 0.2;
-extern const float ang_res_y = 2.0;
-extern const float ang_bottom = 15.0+0.1;
-extern const int groundScanInd = 7;
+// extern const int N_SCAN = 16;
+// extern const int Horizon_SCAN = 1800;
+// extern const float ang_res_x = 0.2;
+// extern const float ang_res_y = 2.0;
+// extern const float ang_bottom = 15.0+0.1;
+// extern const int groundScanInd = 7;
 
 // HDL-32E
 // extern const int N_SCAN = 32;
@@ -104,7 +122,7 @@ extern const int groundScanInd = 7;
 extern const bool loopClosureEnableFlag = false;
 extern const double mappingProcessInterval = 0.3;
 
-extern const float scanPeriod = 0.1;
+// extern const float scanPeriod = 0.1;
 extern const int systemDelay = 0;
 extern const int imuQueLength = 200;
 
