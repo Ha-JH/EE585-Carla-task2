845040a5cd172f190903ba69c1873e29522b46a4
hajh <hajeongho95@gmail.com>
2021-12-19 13:57:49 +0900

    Update utility.h

--- LeGO-LOAM/include/utility.h
+++ LeGO-LOAM/include/utility.h
@@ -69,21 +69,22 @@ extern const bool useCloudRing = true; // if true, ang_res_y and ang_bottom are 
 // extern const float scanPeriod = 0.05;
 
 // type3 and type 4
-extern const int N_SCAN = 128;
-extern const int Horizon_SCAN = 1024;
-extern const float ang_res_x = 360.0/float(Horizon_SCAN);
-extern const float ang_res_y = 45.0/float(N_SCAN-1);
-extern const float ang_bottom = 22.5;
-extern const int groundScanInd = 10;
-extern const float scanPeriod = 0.1;
+// extern const int N_SCAN = 128;
+// extern const int Horizon_SCAN = 1024;
+// extern const float ang_res_x = 360.0/float(Horizon_SCAN);
+// extern const float ang_res_y = 45.0/float(N_SCAN-1);
+// extern const float ang_bottom = 22.5;
+// extern const int groundScanInd = 10;
+// extern const float scanPeriod = 0.1;
 
 // VLP-16
-// extern const int N_SCAN = 16;
-// extern const int Horizon_SCAN = 1800;
-// extern const float ang_res_x = 0.2;
-// extern const float ang_res_y = 2.0;
-// extern const float ang_bottom = 15.0+0.1;
-// extern const int groundScanInd = 7;
+extern const int N_SCAN = 16;
+extern const int Horizon_SCAN = 1800;
+extern const float ang_res_x = 0.2;
+extern const float ang_res_y = 2.0;
+extern const float ang_bottom = 15.0+0.1;
+extern const int groundScanInd = 7;
+extern const float scanPeriod = 0.1;
 
 // HDL-32E
 // extern const int N_SCAN = 32;
