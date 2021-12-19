1f2fa79cfa631aa7debb080e0cf9d3b6bc80d7fe
Ha-JH <hajeongho95@gmail.com>
2021-12-19 14:16:23 +0900

    rostime init

--- LeGO-LOAM/src/mapOptmization.cpp
+++ LeGO-LOAM/src/mapOptmization.cpp
@@ -1527,6 +1527,8 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "lego_loam");
 
+    roa::time::Init();
+
     ROS_INFO("\033[1;32m---->\033[0m Map Optimization Started.");
 
     mapOptimization MO;
